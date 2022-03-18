caebec8bb63f795b0b1bd8cb440fd4de462fbf35
Juan Ignacio Landolfi <juanignaciolandolfi@gmail.com>
2022-03-18 11:47:20 -0300

    Primer Repositorio

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        } else console.log("setState no se llamo");
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error: "No se pudo cargar la info",
+        });
+      });
+  }, [url]);
+
+  return state;
+};
---
+++ useForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export const useForm = (initialState) => {
+  const [values, setValues] = useState(initialState);
+
+  // console.log(values);
+  const reset = () => {
+    setValues(initialState);
+  };
+
+  const handleInputChange = ({ target }) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+  };
+
+  return [values, handleInputChange, reset];
+};
